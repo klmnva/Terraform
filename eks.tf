1ab35ed2b8972cd3e904b87f630fe753c4bc597b
klmnva <klmnva256@gmail.com>
2025-06-23 15:33:54 +0530

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,58 @@
+# EKS Cluster and Node Group
+
+# EKS Cluster
+resource "aws_eks_cluster" "main" {
+  name     = "${var.project_name}-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.28"
+
+  vpc_config {
+    security_group_ids = [aws_security_group.eks_cluster.id]
+    subnet_ids = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id,
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id
+    ]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name = "${var.project_name}-eks-cluster"
+  }
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.project_name}-nodegroup"
+  node_role_arn   = aws_iam_role.eks_nodegroup.arn
+  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+
+  capacity_type  = "ON_DEMAND"
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_container_registry_policy,
+  ]
+
+  tags = {
+    Name = "${var.project_name}-nodegroup"
+  }
+}
